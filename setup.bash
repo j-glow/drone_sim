b1dc5aa181c094c0803b0b34c67642fa075a6047
qbaaa-0 <ntfr123@gmail.com>
2023-04-10 21:12:14 +0200

    update directory for env vars

--- setup.bash
+++ setup.bash
@@ -1,2 +1,2 @@
-export IGN_GAZEBO_RESOURCE_PATH=$HOME/ardupilot_gazebo/models:$HOME/ardupilot_gazebo/worlds:$IGN_GAZEBO_RESOURCE_PATH
-export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$HOME/ardupilot_gazebo/build:$IGN_GAZEBO_SYSTEM_PLUGIN_PATH
+export IGN_GAZEBO_RESOURCE_PATH=$HOME/drone_sim/models:$HOME/drone_sim/worlds:$IGN_GAZEBO_RESOURCE_PATH
+export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$HOME/drone_sim/build:$IGN_GAZEBO_SYSTEM_PLUGIN_PATH
