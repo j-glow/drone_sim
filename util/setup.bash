0b780c865d94a7143bd0b05bfae4b972a9ec3489
qbaaa-0 <ntfr123@gmail.com>
2023-04-11 22:53:24 +0200

    updated setup script to work in all paths

--- util/setup.bash
+++ util/setup.bash
@@ -1,2 +1,8 @@
-export IGN_GAZEBO_RESOURCE_PATH=$HOME/drone_sim/models:$HOME/drone_sim/worlds:$IGN_GAZEBO_RESOURCE_PATH
-export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$HOME/drone_sim/build:$IGN_GAZEBO_SYSTEM_PLUGIN_PATH
+#!/bin/bash
+
+script_path=`readlink -f “${BASH_SOURCE:-$0}”`
+DIR_PATH=`dirname $script_path`
+SIM_PATH=`dirname $DIR_PATH`
+
+export IGN_GAZEBO_RESOURCE_PATH=$SIM_PATH/models:$SIM_PATH/worlds:$IGN_GAZEBO_RESOURCE_PATH
+export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$SIM_PATH/build:$IGN_GAZEBO_SYSTEM_PLUGIN_PATH
