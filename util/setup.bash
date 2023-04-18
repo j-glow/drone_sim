2193ba6eac3125c3c9672b8f1f41ce2a3f90ebda
qbaaa-0 <ntfr123@gmail.com>
2023-04-18 23:05:03 +0200

    fix missing operand dirname error

--- util/setup.bash
+++ util/setup.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 script_path=`readlink -f “${BASH_SOURCE:-$0}”`
-DIR_PATH=`dirname $script_path`
-SIM_PATH=`dirname $DIR_PATH`
+DIR_PATH=`dirname "$script_path"`
+SIM_PATH=`dirname "$DIR_PATH"`
 
 export IGN_GAZEBO_RESOURCE_PATH=$SIM_PATH/models:$SIM_PATH/worlds:$IGN_GAZEBO_RESOURCE_PATH
 export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$SIM_PATH/build:$IGN_GAZEBO_SYSTEM_PLUGIN_PATH
